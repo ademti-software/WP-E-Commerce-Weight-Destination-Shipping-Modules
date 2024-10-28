073a2db72acd4e5ce05eb1f0a2c90b2dd18c879d
Lee Willis <lee@ademti-software.co.uk>
2024-10-28 14:33:34 +0000

    Ownership & copyright

--- wp-e-commerce-weight-region-shipping.php
+++ wp-e-commerce-weight-region-shipping.php
@@ -1,19 +1,35 @@
 <?php
 /*
  Plugin Name: WP E-Commerce Weight & Destination Shipping Modules
- Plugin URI: http://www.leewillis.co.uk/wordpress-plugins/
+ Plugin URI: https://www.ademti-software.co.uk/
  Description: Shipping Modules For WP E-Commerce bases prices on region (Continent, or country / region) and weight bands
  Version: 5.0
- Author: Lee Willis
- Author URI: http://www.leewillis.co.uk/
+ Author: Ademti Software
+ Author URI: https://www.ademti-software.co.uk/
 */
 
-/*
- This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 2.
- This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+/**
+ * Copyright (c) 2010-2015 Lee Willis. All rights reserved.
+ * Copyright (c) 2015-2024 Ademti Software. All rights reserved.
+ *
+ * Released under the GPL license
+ * http://www.opensource.org/licenses/gpl-license.php
+ *
+ * This is an add-on for WordPress
+ * http://wordpress.org/
+ *
+ * **********************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * **********************************************************************
  */
-
-
 
 /**
  * wp-e-commerce-weight-region-shipping.php
